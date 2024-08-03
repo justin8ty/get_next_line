eb8ebab01b14fb21213e988f9ca71aeecb5acc5b
Justin Tan <justin8ty@gmail.com>
2024-08-04 07:11:05 +0800

    chore: Add header file

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jin-tan <jin-tan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 20:21:43 by jin-tan           #+#    #+#             */
+/*   Updated: 2024/08/04 07:05:27 by jin-tan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+
+# endif
+
+char	*get_next_line(int fd);
+char	*read_to_stash(int fd, char *stash);
+char	*read_to_line(char *stash);
+char	*update_stash(char *stash);
+
+// Utils
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+
+#endif
