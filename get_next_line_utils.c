7c7bc61f1ac93255275ced49e56a7666ea62f285
Justin Tan <justin8ty@gmail.com>
2024-08-04 07:07:41 +0800

    feat: Add utils

---
+++ get_next_line_utils.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jin-tan <jin-tan@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 20:41:17 by jin-tan           #+#    #+#             */
+/*   Updated: 2024/08/04 07:07:15 by jin-tan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+// ft_strchr parameters: a string and character to search for.
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	cc;
+
+	cc = (char)c;
+	i = 0;
+	while (s[i])
+	{
+		if (cc == s[i])
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (cc == s[i])
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+	str[j] = '\0';
+	return (str);
+}
